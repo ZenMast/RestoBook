9204b16a2a8f17cf19c5f9c047cb5baeb05fe0ce
Roman Shumailov <rmshum@gmail.com>
2015-03-08 22:58:58 +0200

    Fixing

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ echo 'Hey';
 //defined('YII_DEBUG') or define('YII_DEBUG', true);
 //defined('YII_ENV') or define('YII_ENV', 'dev');
 error_reporting(-1);
-// ini_set('display_errors', true);
+ini_set('display_errors', true);
 
 require(__DIR__ . '/_protected/vendor/autoload.php');
 require(__DIR__ . '/_protected/vendor/yiisoft/yii2/Yii.php');
