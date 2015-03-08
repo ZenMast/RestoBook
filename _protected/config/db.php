3921f9d4afe966c489fd353cf6248397cacfeb02
Roman Shumailov <rmshum@gmail.com>
2015-03-08 21:15:57 +0200

    Configured database credentials

--- _protected/config/db.php
+++ _protected/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=basic',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=eu-cdbr-azure-west-a.cloudapp.net;dbname=bronRestData',
+    'username' => 'bbf31321b05540',
+    'password' => '3173654a',
     'charset' => 'utf8',
 ];
