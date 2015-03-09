039a2fea45c6f244bbfe6c4f279e2c280e1b9b9d
Roman Shumailov <rmshum@gmail.com>
2015-03-09 02:16:02 +0200

    Fixing

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
-<h1>
-TESTING TESTING TESTING
-</h1>
 <?php
-echo 'Hey';
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 // defined('YII_ENV') or define('YII_ENV', 'dev');
 error_reporting(-1);
 ini_set('display_errors', true);
 
-require(__DIR__ . '/_protected/vendor/autoload.php');
-require(__DIR__ . '/_protected/vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/protected/vendor/autoload.php');
+require(__DIR__ . '/protected/vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/_protected/config/web.php');
+$config = require(__DIR__ . '/protected/config/web.php');
 
 (new yii\web\Application($config))->run();
