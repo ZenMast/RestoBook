fba557c89597eacc6a2f39991f77ae45512ee77e
Roman Shumailov <rmshum@gmail.com>
2015-03-13 18:24:28 +0200

    Start ignoring db.php for security purposes

--- _protected/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=eu-cdbr-azure-west-a.cloudapp.net;dbname=bronRestData',
-    'username' => 'bbf31321b05540',
-    'password' => '3173654a',
-    'charset' => 'utf8',
-];
