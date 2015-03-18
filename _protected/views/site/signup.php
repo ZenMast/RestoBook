759aeeab5824c5cc2ce4acad5c0676f7f1371d18
Deva Olga <olgsa678@gmail.com>
2015-03-19 01:51:07 +0200

    Signup and login
    JS - facebook.js - Facebook SDK + loadData() which fills fields with
    facebook data in sign up page
    Signup -  added phone, name, repeat password
    Created login widget - inline login form (currently commented out in
    main)

--- _protected/views/site/signup.php
+++ _protected/views/site/signup.php
@@ -3,6 +3,7 @@ use nenad\passwordStrength\PasswordInput;
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
+
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \app\models\SignupForm */
@@ -16,20 +17,24 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="col-lg-5 well bs-component">
 
-        <p><?= Yii::t('app', 'Please fill out the following fields to signup:') ?></p>
-
-        <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
-
-            <?= $form->field($model, 'username') ?>
+        <p><?= Yii::t('app', 'Please fill out the following fields to signup:') ?></p>	 
+	   <div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="true" data-auto-logout-link="false"  data-scope="email,public_profile"></div>
+        <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>			
+		    <?= $form->field($model, 'username') ?>
             <?= $form->field($model, 'email') ?>
             <?= $form->field($model, 'password')->widget(PasswordInput::classname(), []) ?>
-
+            <?= $form->field($model, 'password2')->passwordInput() ?>            
+            <?= $form->field($model, 'name') ?>                
+            <?= $form->field($model, 'phone') ?>
+            <?= $form->field($model, 'facebook_id')->hiddenInput()->label(''); ?>
             <div class="form-group">
                 <?= Html::submitButton(Yii::t('app', 'Signup'), ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
             </div>
 
+		
         <?php ActiveForm::end(); ?>
-
+        
+		
         <?php if ($model->scenario === 'rna'): ?>
             <div style="color:#666;margin:1em 0">
                 <i>*<?= Yii::t('app', 'We will send you an email with account activation link.') ?></i>
