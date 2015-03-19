7734b163d18a90af785ab6a08076d97f720e3ac1
Deva Olga <olgsa678@gmail.com>
2015-03-19 02:01:20 +0200

    .gitignore is now working

--- _protected/config/db.php
+++ _protected/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=eu-cdbr-azure-west-a.cloudapp.net;dbname=bronRestData',
-    'username' => 'bbf31321b05540',
-    'password' => '3173654a',
+    'dsn' => 'mysql:host=localhost;dbname=yii3',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
