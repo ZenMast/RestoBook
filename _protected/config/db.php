5b817a53b3a6f05b77abb383561731af51e1ca2f
Deva Olga <olgsa678@gmail.com>
2015-03-19 02:06:35 +0200

    Revert ".gitignore is now working"
    This reverts commit 7734b163d18a90af785ab6a08076d97f720e3ac1.

--- _protected/config/db.php
+++ _protected/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii3',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=eu-cdbr-azure-west-a.cloudapp.net;dbname=bronRestData',
+    'username' => 'bbf31321b05540',
+    'password' => '3173654a',
     'charset' => 'utf8',
 ];
