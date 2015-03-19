5ba6d149a8ed72553e4f10b2e7e7ec4a433383d5
Roman Shumailov <rmshum@gmail.com>
2015-03-19 18:46:23 +0200

    Trying to fix deployment

--- deploy.sh
+++ deploy.sh
@@ -82,6 +82,7 @@ echo Handling Basic Web Site deployment.
 
 # 1. KuduSync
 if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
+  echo Wazup
   "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
   exitWithMessageOnError "Kudu Sync failed"
 fi
