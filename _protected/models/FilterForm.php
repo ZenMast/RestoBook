fd91d7a355b13203ab62e7e892b85554516b5a8c
Deva Olga <olgsa678@gmail.com>
2015-04-16 01:16:54 +0300

    Filter improve
    Filters all stuff, doesn't reset picked value in dropdown.
    Removed ajax dropdown

--- _protected/models/FilterForm.php
+++ _protected/models/FilterForm.php
@@ -11,9 +11,10 @@ class FilterForm extends Model
 	public $city;
 	public $opening_time;
 	public $closing_time;
+	public $restaurants;
 	public $restaurant;
 	public $cuisine;
-	public $guests;
+	public $guests = 2;
 	public $booking_time;
 	public $date;
 
@@ -21,8 +22,13 @@ class FilterForm extends Model
 
     public function rules()
     {
+
         return [
-            
+            ['country', 'string'],
+            ['city', 'string'], 
+        	['restaurant', 'string'],
+        	['cuisine', 'string'],
+        	['guests', 'number'],
         ];
     }
     public function attributeLabels()
--- _protected/views/site/index.php
+++ _protected/views/site/index.php
@@ -21,68 +21,33 @@ $this->title = Yii::t('app', Yii::$app->name);
 <div class="site-index">
     <div class="body-content">
         <div class="row">
-            <div class="col-md-2">
-                <div id="ajaxusage">
-                   <!--     Ajax usage -->
-                   <?php echo Html::beginForm('', 'post', ['class'=>'uk-width-medium-1-1 uk-form uk-form-horizontal']); ?>
-               
-                       <?= Select2::widget([
-                           'name' => 'resto_id',
-                           'data' => RestaurantSearch::findAllIdsToAssocString(),
-                           'options' => [
-                               'id' => 'resto_select',
-                               'multiple' => false, 
-//                                 'placeholder' => 'Choose...',
-                               'class' => 'uk-width-medium-7-10']
-                            ]);
-                       ?>
-                       
-                       Click for Ajax
-                       <?php AjaxSubmitButton::begin([
-                           'label' => 'Check',
-                           'ajaxOptions' => [
-                               'type'=>'POST',
-                               'url' => 'index.php?r=site%2Fgetinfo',
-                               /*'cache' => false,*/
-                               'success' => new \yii\web\JsExpression('function(html){
-                                   $("#output").html(html);
-                                   }'),                
-                           ],
-                           'options' => ['class' => 'customclass', 'type' => 'submit'],
-                           ]);
-                           AjaxSubmitButton::end();
-                       ?>                       
-                       <?php echo Html::endForm(); ?>
-                       </div> 
+            <div class="col-md-2">               
                     <?php $form = ActiveForm::begin([
-                    	'method' => 'get',
-    					'action' => Url::to(['site/filter']),
-                        //'action' => '/mailing-list/index',
-                        ]); ?>
-                    <?= $form->field($model, 'country')->dropDownList($country, ['prompt'=>'--Country--']) ?>
-                    <?= $form->field($model, 'city')->dropDownList($city, ['prompt'=>'--City--']) ?>                 
-                    <?= $form->field($model, 'restaurant')->dropDownList($restaurant, ['prompt'=>'--Restaurant--']) ?>                  
-                    <?= $form->field($model, 'cuisine')->dropDownList($cuisines, ['prompt'=>'--Cuisine--']) ?> 
-                    <?= $form->field($model, 'date')-> widget(TimePicker::className(), [ 'name'  => 'book_date','mode' => 'date']) ?>    
-                    <?= $form->field($model, 'booking_time')-> widget(TimePicker::className(), [ 'name'  => 'book_time','mode' => 'time']) ?>
+                        ]); ?>            
+                    <?= $form->field($model, 'country')->dropDownList(["All Countries"=>"All Countries"] + ArrayHelper::map($restaurants, 'country', 'country'),  ['options' =>[$model->country => ['selected ' => true]]]) ?>
+                    <?= $form->field($model, 'city')->dropDownList(["All Cities"=>"All Cities"] + ArrayHelper::map($restaurants, 'city', 'city'),  ['options' =>[$model->city => ['selected ' => true]]]) ?>         
+                    <?= $form->field($model, 'restaurant')->dropDownList(["All Restaurants"=>"All Restaurants"] + ArrayHelper::map($restaurants, 'name', 'name'),  ['options' =>[$model->restaurant => ['selected ' => true]]]) ?>                  
+                    <?= $form->field($model, 'cuisine')->dropDownList(["All Cuisines"=>"All Cuisines"] + array_filter(ArrayHelper::map($restaurants, 'cuisine', 'cuisine')),  ['options' =>[$model->cuisine => ['selected ' => true]]]) ?>                  
+                    <?= $form->field($model, 'date')-> widget(TimePicker::className(), [ 'name'  => 'book_date','mode' => 'date',  'options'=> ['dateFormat'=>'dd-mm-yy','value'=>date('d-m-Y')]]) ?>       
+                    <?= $form->field($model, 'booking_time')-> widget(TimePicker::className(), [ 'name'  => 'book_time','mode' => 'time', 'options'=> ['dateFormat'=>'dd-mm-yy','value'=>date('H:i:s')]]) ?>
                     <?= $form->field($model, 'guests') ?>              	              
                		<?= Html::submitButton(Yii::t('app', 'Filter'), ['class' => 'btn btn-primary']) ?>
                     <?php ActiveForm::end(); ?>                          
 			</div> 
             <div class="col-md-10">
             <div class="container-fluid">
-            <?php for ($i = 0; $i < count($restaurants); ++$i){ ?>
+            <?php for ($i = 0; $i < count($selected_restaurants); ++$i){ ?>
             <?php if ($i % 3 == 0){echo '<div class="row">';} ?>
             <div class="col-md-4">
                     <?php $img = Html::img('http://www.92y.org/92streety/media/classes_events/food_drink/lg/food_whiskey_lg.jpg', ['width' => '250'])?>
-                    <div class="row"><?= Html::a($img, ['site/table_selection', 'restaurant'=>$restaurants[$i]->name]) ?></div>
-                    <div class="row"><?= Html::Label('Restaurant: '.$restaurants[$i]->name ) ?></div>
-                    <div class="row"><?= Html::label('Location: '.$restaurants[$i]->address)?></div>   
-                    <div class="row"><?= Html::label('Opened: '.$restaurants[$i]->opening_time.'-'.$restaurants[$i]->closing_time)?></div> 
-                    <?php if(!empty($restaurants[$i]->cuisine)){echo '<div class="row">'.Html::label('Kitchen: '.$restaurants[$i]->cuisine).'</div>';} ?>
-                    <?php if(!empty($restaurants[$i]->max_people)){echo '<div class="row">'.Html::label('Max: '.$restaurants[$i]->max_people).'</div>';} ?>                                                    
+                    <div class="row"><?= Html::a($img, ['site/table_selection', 'restaurant'=>$selected_restaurants[$i]->name]) ?></div>
+                    <div class="row"><?= Html::Label('Restaurant: '.$selected_restaurants[$i]->name ) ?></div>
+                    <div class="row"><?= Html::label('Location: '.$selected_restaurants[$i]->country.", ".$selected_restaurants[$i]->city.", ".$selected_restaurants[$i]->address)?></div>   
+                    <div class="row"><?= Html::label('Opened: '.$selected_restaurants[$i]->opening_time.'-'.$selected_restaurants[$i]->closing_time)?></div> 
+                    <?php if(!empty($selected_restaurants[$i]->cuisine)){echo '<div class="row">'.Html::label('Kitchen: '.$selected_restaurants[$i]->cuisine).'</div>';} ?>
+                    <?php if(!empty($selected_restaurants[$i]->max_people)){echo '<div class="row">'.Html::label('Max: '.$selected_restaurants[$i]->max_people).'</div>';} ?>                                                   
             </div>
-            <?php if (($i + 1) % 3 == 0 ||$i == sizeof($restaurants)){echo '</div>';} ?>           	
+            <?php if (($i + 1) % 3 == 0 ||$i == sizeof($selected_restaurants)){echo '</div>';} ?>           	
             <?php } ?>	
             </div>	
           </div>	
